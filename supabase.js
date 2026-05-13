a5b95fa1f86303694d4c92e8f6ebc0ee25a182f1
Rakoto Anjaramamy <rakotoanjaramamy@gmail.com>
2026-05-13 17:11:09 +0300

    feat: connexion Supabase

---
+++ supabase.js
@@ -0,0 +1,4 @@
+const supabaseUrl = 'https://crxofkxinsspfgdsxpiy.supabase.co'
+const supabaseKey = 'sb_publishable_oRZBgjE_IWkCWn6glpie2A_ymVzz1Uj'
+
+const supabase = window.supabase.createClient(supabaseUrl, supabaseKey)
